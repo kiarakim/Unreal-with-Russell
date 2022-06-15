a1b90f65f64528bf0395a4fe79c34202575a23c9
kiara kim <kiarakim413@gmail.com>
2022-06-15 23:52:02 +0900

    add 2292. 벌집

---
+++ 단계별89/2292. 벌집.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int cnt = 1;
+		int range = 2;
+		
+		if(N == 1)
+			System.out.println(1);
+		else {
+			while(range <= N) {
+				range += cnt * 6;
+				cnt++;
+			}
+			System.out.println(cnt);
+		}
+	}
+}
