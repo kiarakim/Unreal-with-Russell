8ceda71eae20f08c3b4687cf1e60cd10815ccfa9
Kiara Kim <kiaraKim413@gmail.com>
2022-07-02 21:40:09 +0900

    add 1000. A+B

--- 단계별89/1000. A+B.java
+++ 단계별89/1000. A+B.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main (String[] args) {
 		Scanner in = new Scanner(System.in);
 		int A = in.nextInt();
-		int B = in.nextInt();
+		int B = in.nextInt(); 
 		
 		System.out.println(A+B);
 		
